76b0bd5fbed76a4972847ebee0a4d5232f4855e5
Konrad Kalemba <konrad_kal@hotmail.com>
2023-12-03 14:34:09 -0500

    feat: tidy up sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { auth, signIn } from "@/lib/auth";
 import { redirect } from "next/navigation";
@@ -15,8 +16,13 @@ export default async function SignIn() {
         "use server";
         await signIn("github", { redirectTo: "/onboarding" });
       }}
+      className="mx-auto w-[240px] flex flex-col gap-4 pt-12"
     >
-      <Button type="submit">Sign in with GitHub</Button>
+      <Logo />
+      <h1 className="text-2xl">Get started</h1>
+      <Button type="submit" className="bg-gray-900">
+        Sign in with GitHub
+      </Button>
     </form>
   );
 }
